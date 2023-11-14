5eb72104ecc5aa2d2391e2bcb31b9455485646bf
Rasmus Eng <rasmus.eng@elev.ga.ntig.se>
2023-11-14 11:08:02 +0100

    börjat på spelet

--- VinterProjekt/Program.cs
+++ VinterProjekt/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Random generator = new Random();
+string[] names = {"Pickachu", "Mewtwo","Charizard","Bulbausar"};
+string[] names2 ={""};
+string[] names3 ={""};
+//Tre listor med namn med olika pokemons
+
+int Card1 = generator.Next(4);
+int Card2 = generator.Next(4);
+int Card3 = generator.Next(4);
+//Tar en pokemon random från varje lista
+
+Console.WriteLine("Welcome to Pokemon Card Battle\nChoose a Fighter!");
+Console.WriteLine(names[Card1]);
+Console.WriteLine(names2[Card2]);
+Console.WriteLine(names3[Card3]);
+Console.ReadLine();
+    
